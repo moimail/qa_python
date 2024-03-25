05c13cb29690dce2b089df2d9223b0e4e6be0e40
Konstantin Tiupin <moimial@yandex.ru>
2024-03-26 00:02:52 +0300

    qa_python

--- tests.py
+++ tests.py
@@ -22,3 +22,58 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+
+        assert "Гордость и предубеждение и зомби" in collector.get_books_genre()
+
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+
+        assert collector.get_book_genre("Гордость и предубеждение и зомби") == "Фантастика"
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Война и мир")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.set_book_genre("Война и мир", "Драма")
+
+        assert collector.get_books_with_specific_genre("Фантастика") == ["Гордость и предубеждение и зомби"]
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+
+        assert collector.get_books_genre() == {"Гордость и предубеждение и зомби": "Фантастика"}
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Война и мир")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.set_book_genre("Война и мир", "Драма")
+
+        assert collector.get_books_for_children() == []
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.add_book_in_favorites("Гордость и предубеждение и зомби")
+
+        assert collector.get_list_of_favorites_books() == ["Гордость и предубеждение и зомби"]
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.add_book_in_favorites("Гордость и предубеждение и зомби")
+        collector.delete_book_from_favorites("Гордость и предубеждение и зомби")
+
+        assert collector.get_list_of_favorites_books() == []
